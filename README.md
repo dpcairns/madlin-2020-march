dd6f38c3411b373407d4ff69ba633a36c3dfddec
Danny Cairns <dpcairns@gmail.com>
2020-03-27 14:28:07 -0700

    add new instructions

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
-# madlin-2020-march
+# madlib-2020-march
 
-Instructions, part one
+### Instructions, part one
 1) Make a repo and clone it locally
-1) Add index.html, style.css, and app.js.
+1) Add `index.html`, `style.css`, and `app.js`.
 1) Add screenshot of the wireframes
 1) Use comment-driven development to describe what needs to happen, piece by piece, in each file
-1) Add a madlin container and a input container.
+1) Add a madlib container and a input container.
 1) In your input container, add one input and one button with an id to you HTML. Both should have IDs
-1) In app.js, get the input and button by id.
+1) In `app.js`, get the input and button by id.
 1) Add an event listener that logs the value of the input.
+
+### Instructions, part two
+1) In `index.html` Add a `span` tag with an id to your madlib container. This is a place to inject the value of your input.
+1) In `app.js`, get the `span`, and SET the textContent to the value of the input in our click handler.
+1) In `app.js`, get the input container, and hide it in our click handler.
+1) In `style.css`, set the madlib container to hidden fro, the get go.
+1) In `app.js`, set the madlib container to visible in the click hanlder.
--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 // GET A BUNCH OF DOM ELEMENTS
 
 // go get html elements by their ids
-
 // we'll need all the inputs to get the words FROM the user
 const riverInput = document.getElementById('river-name-input');
 // we'll need spans to put the WORDS INTO the madlib
-
+const riverSpan = document.getElementById('river-name-span');
 // we'll need the container that holds the mad lib
-
+const madlibContainer = document.getElementById('madlib-container');
 // we'll need the container that holds all the inputs
-
+const inputContainer = document.getElementById('input-container');
 // we'll need button so we can add the event listener
 const myButton = document.getElementById('button');
 
@@ -19,11 +18,12 @@ function putAnswersIn() {
     // this event listener should . . .
     // GET the value of the inputs
     const riverInputValue = riverInput.value;
-    // use the input values to SET the value of our spans
-    console.log('something', riverInputValue);
+    // use the input values to SET the textContent of our spans
+    riverSpan.textContent = riverInputValue;
     // hide the input container
+    inputContainer.style.display = 'none';
     // reveal the mad lib container
-
+    madlibContainer.style.display = 'block';
 }
 
 // add event listener to the button on the CLICK event
